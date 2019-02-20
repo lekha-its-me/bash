04ffb2c97e11e0cb5df22be2511691e91c7ebe01
Alexey Baranov <lekha.baranov@gmail.com>
2019-02-20 11:41:19 +0200

    update script

--- restore_backup.sh
+++ restore_backup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-BACKUPS_ROOT=backups
-RESTORES_ROOT=./restores/
 
-if [ -d $BACKUPS_ROOT/$1 ]; 
+set -e
+if [ $1=~^[0-9]{2}-[0-9]{2}-[0-9]{4}$ ];
 then
-	echo "Path exist..."
-	cp backups/$1/dump.sql.gz restores/
-	cp backups/$1/uploads.zip restores/
-	gunzip restores/dump.sql.gz
-	unzip restores/uploads.zip -d restores/
-	rm restores/uploads.zip
+	if [ -d $BACKUPS_ROOT/$1 ]; 
+	then
+		echo "Path exist..."
+		cp backups/$1/dump.sql.gz backend/
+		cp backups/$1/uploads.zip backend/web/uploads/
 
-	echo "Backup for $1 restored"
+		gunzip backend/dump.sql.gz
+		unzip backend/web/uploads/uploads.zip -d backend/web/uploads/
+		rm restores/uploads.zip
+
+		echo "Backup for $1 restored"
    
+	else
+		echo "Backup for $1 not exist"
+	fi
 else
-	echo "Backup for $1 not exist"
+	echo "Wrong date format "$1
 fi
